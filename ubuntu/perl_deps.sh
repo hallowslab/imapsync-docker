dde64e2e5b97082c92f02eb1eaf9cb3a2c089847
hallowslab <hallows.tech.lab@gmail.com>
2025-08-14 17:11:44 +0100

    Ubuntu images

---
+++ ubuntu/perl_deps.sh
@@ -0,0 +1,7 @@
+set -e
+apt install -y libauthen-ntlm-perl libclass-load-perl libcrypt-openssl-rsa-perl libcrypt-ssleay-perl libdata-uniqid-perl libdigest-hmac-perl \
+    libdist-checkconflicts-perl libencode-imaputf7-perl libfile-copy-recursive-perl libfile-tail-perl libio-compress-perl libio-socket-inet6-perl \
+    libio-socket-ssl-perl libio-tee-perl libjson-webtoken-perl libmail-imapclient-perl libmodule-scandeps-perl libnet-dbus-perl libnet-ssleay-perl \
+    libpar-packer-perl libproc-processtable-perl libreadonly-perl libregexp-common-perl libsys-meminfo-perl libterm-readkey-perl libtest-fatal-perl \
+    libtest-mock-guard-perl libtest-mockobject-perl libtest-pod-perl libtest-requires-perl libtest-simple-perl libunicode-string-perl liburi-perl \
+    libtest-nowarnings-perl libtest-deep-perl libtest-warn-perl libwww-perl libcgi-pm-perl
