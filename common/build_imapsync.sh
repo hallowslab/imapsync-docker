1f567720d86fc367a4baf032ef65f1217013f8c8
hallowslab <hallows.tech.lab@gmail.com>
2025-08-14 13:17:10 +0100

    Add common scripts

---
+++ common/build_imapsync.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+VER="$1"
+mkdir -p /opt/imapsync
+git clone https://github.com/imapsync/imapsync.git /opt/imapsync
+cd /opt/imapsync
+git checkout imapsync-$VER
+make install
