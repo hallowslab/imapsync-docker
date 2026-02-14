31f17676855e5955bf1e540689f0cf307ca6b2f5
hallowslab <hallow.tech.lab@gmail.com>
2026-02-14 13:55:02 +0000

    debian move perl_deps to global for now

--- debian/bullseye/perl_deps.sh
+++
@@ -1,7 +0,0 @@
-set -e
-# Perl deps
-apt install -y libauthen-ntlm-perl libcgi-pm-perl libcrypt-openssl-rsa-perl libcrypt-openssl-pkcs12-perl libdata-uniqid-perl libencode-imaputf7-perl libfile-copy-recursive-perl \
- libfile-tail-perl libio-compress-perl libio-socket-inet6-perl libio-socket-ssl-perl libio-tee-perl libhtml-parser-perl libjson-webtoken-perl libmail-imapclient-perl \
- libparse-recdescent-perl libproc-processtable-perl libmodule-scandeps-perl libreadonly-perl libregexp-common-perl libsys-meminfo-perl libterm-readkey-perl \
- libtest-mockobject-perl libtest-pod-perl libunicode-string-perl liburi-perl libwww-perl libtest-nowarnings-perl libtest-deep-perl libtest-warn-perl \
- libdist-checkconflicts-perl libpar-packer-perl libtest-fatal-perl libtest-mock-guard-perl libtest-requires-perl
